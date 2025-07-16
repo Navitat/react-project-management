d3f076c510eb4fd3ab54824b4b8f6079d3e2bc07
navitat <anlon636@gmail.com>
2025-07-16 09:55:21 +0200

    create pages and components folders with principal components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+function Navbar() {
+  return <h1>Welcome to Project Management</h1>;
+}
+
+export default Navbar;
---
+++ src/pages/ProjectListPage.jsx
@@ -0,0 +1,9 @@
+function ProjectListPage() {
+  return (
+    <div>
+      <h1>List of Prjects...</h1>
+    </div>
+  );
+}
+
+export default ProjectListPage;
