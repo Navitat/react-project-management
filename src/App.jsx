90a0fa07feb2cc447a779ee2a3b3e42a2ce5ff91
navitat <anlon636@gmail.com>
2025-07-16 10:48:26 +0200

    feat: implement list of projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import ProjectListPage from "./pages/PRojectListPage";
+
 function App() {
   return (
     <>
       <h1>React Project Management</h1>
+      <ProjectListPage />
     </>
   );
 }
--- src/pages/ProjectListPage.jsx
+++ src/pages/ProjectListPage.jsx
@@ -1,7 +1,36 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Loader from "../components/Loader";
+
+const BASE_URL = "https://project-management-api-4641927fee65.herokuapp.com";
+
 function ProjectListPage() {
+  const [projects, setProjects] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(BASE_URL + "/projects")
+      .then((response) => {
+        setProjects(response.data);
+      })
+      .catch((error) => console.log("ERROR: cannot GET projects", error));
+  }, []);
+
+  if (projects === null) {
+    return <Loader />;
+  }
+
   return (
     <div>
-      <h1>List of Prjects...</h1>
+      <h2>Number of Projects: {projects.length}</h2>
+      {projects.map((project) => {
+        return (
+          <div key={project.id} className="card">
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
